f4fe0161d1664da58a1d09ef55a64f351f6158d2
Alaa Eddine <alaaeddine.elmoudden@reply.it>
2026-09-08 11:23:31 +0200

    feat: implement Zod schemas for new user and registered user validation + inferred types to avoid duplicates, stay in sync and single source of truth

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -1,5 +1,22 @@
-export const validateEmail = (email: string) => {
-  const regex =
-    /^[a-z\d!#$%&'*+\-/=?^_`{|}~]+(?:\.[a-z\d!#$%&'*+\-/=?^_`{|}~]+)*@(?:[a-z\d](?:[a-z\d-]*[a-z\d])?\.)+[a-z]{2}$/i;
-  return regex.test(String(email).toLowerCase());
-};
+import { z } from "zod";
+
+export const newUserSchema = z.object({
+  name: z.string().min(1, { error: "Invalid name" }),
+  email: z.email({ error: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { error: "Password must be at least 8 characters long" }),
+  confirmPassword: z
+    .string()
+    .min(8, { error: "Confirm Password is not correct" }),
+});
+
+export const registeredUserSchema = z.object({
+  email: z.email({ error: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { error: "Password must be at least 8 characters long" }),
+});
+
+export type NewUser = z.infer<typeof newUserSchema>;
+export type RegisteredUser = z.infer<typeof registeredUserSchema>;
