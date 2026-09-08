121a331f76e7e388c5e05d3fa32b83992a157283
Alaa Eddine <alaaeddine.elmoudden@reply.it>
2026-09-08 10:44:00 +0200

    refactor: replace custom useAppFonts hook with direct use of useFonts in RootLayout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,25 @@
-import { useAppFonts } from "@/hooks/useAppFonts";
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  const ready = useAppFonts();
-  if (!ready) return null;
+  const [loaded] = useFonts({
+    "NunitoSans-Regular": require("../../assets/fonts/NunitoSans-Regular.ttf"),
+    "NunitoSans-Medium": require("../../assets/fonts/NunitoSans-Medium.ttf"),
+    "NunitoSans-SemiBold": require("../../assets/fonts/NunitoSans-SemiBold.ttf"),
+    "NunitoSans-Bold": require("../../assets/fonts/NunitoSans-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <>
