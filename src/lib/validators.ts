23a0afe972a50df65a1a1e5b4a0de4cc5c5a7dfb
Alaa Eddine <alaaeddine.elmoudden@reply.it>
2026-09-21 12:42:40 +0200

    refactor: improve formatting of SignUpUserSchema for better readability

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -1,15 +1,20 @@
 import { z } from "zod";
 
-export const SignUpUserSchema = z.object({
-  user_name: z.string().min(2, { message: "Invalid name" }),
-  email: z.email({ message: "Invalid email address" }),
-  password: z
-    .string()
-    .min(8, { message: "Password must be at least 8 characters long" }),
-  confirmPassword: z
-    .string()
-    .min(8, { message: "Password confirmation is not correct" }),
-});
+export const SignUpUserSchema = z
+  .object({
+    user_name: z.string().min(2, { message: "Invalid name" }),
+    email: z.email({ message: "Invalid email address" }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Password confirmation is not correct" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 export const LoginUserSchema = z.object({
   email: z.email({ message: "Invalid email address" }),
